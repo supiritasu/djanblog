eede6d185d437ba9370585e38729f8c851f93a0c
supiritasu <supiritasu.supiritasu@gmail.com>
2024-02-10 10:14:32 +0900

    qe

---
+++ microblog/local_settings.py
@@ -0,0 +1,8 @@
+#Gmail
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'poaro.doil.poaro@gmail.com'
+EMAIL_HOST_PASSWORD = 'poaro4869'    
+EMAIL_HOST_PASSWORD = 'aqqp xqds xevm ytyu'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
--- microblog/settings.py
+++ microblog/settings.py
@@ -167,11 +167,7 @@ SUPERUSER_EMAIL = env("SUPERUSER_EMAIL")
 SUPERUSER_PASSWORD = env("SUPERUSER_PASSWORD")
 
 
-#Gmail
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'poaro.doil.poaro@gmail.com'
-EMAIL_HOST_PASSWORD = 'poaro4869'    
-EMAIL_HOST_PASSWORD = 'aqqp xqds xevm ytyu'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+try:
+    from .local_settings import *
+except ImportError:
+    pass
