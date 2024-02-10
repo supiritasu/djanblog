a7991ca60dde8003c01e92969750ebbcef73bf75
supiritasu <supiritasu.supiritasu@gmail.com>
2024-02-10 17:55:16 +0900

    basic

--- microblog/wsgi.py
+++ microblog/wsgi.py
@@ -10,6 +10,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from wsgi_basic_auth import BasicAuth
+application = BasicAuth(application)
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'microblog.settings')
 
