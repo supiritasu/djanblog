f8f7807804854ef5b48c4e04cb567b17aca38708
supiritasu <supiritasu.supiritasu@gmail.com>
2024-02-11 15:27:08 +0900

    url

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     # 他のURLパターン
     path('post/<int:post_id>/edit/', views.edit_post, name='edit_post'),
-    path('googled6cb1cfdea5ad30b.html/', views.google_search_console, name='google_search_console'),
+    
 ]
--- microblog/urls.py
+++ microblog/urls.py
@@ -3,13 +3,13 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from blogapp.views import frontpage, post_detail
+from blogapp.views import frontpage, post_detail, google_search_console
 
 urlpatterns = [
     path('admin/', admin.site.urls), #URLがxxx.comの時，xxx.com/admin
     path("", frontpage),
     path("<slug:slug>/", post_detail, name="post_detail"), #xxx.com/{slug}とし，post_detailを返す(slugは投稿番号)
-
+    path('googled6cb1cfdea5ad30b.html/', google_search_console, name='google_search_console'),
 ]
 
 if settings.DEBUG:
