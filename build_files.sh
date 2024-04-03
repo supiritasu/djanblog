eec11664f32fdc1c2842237afc8d819b72da8f8a
supiritasu <supiritasu.supiritasu@gmail.com>
2024-04-03 16:07:55 +0900

    hogehoge

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,3 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
+pip3 install -r requirements.txt
+python3.9 manage.py collectstatic
 
-pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-python manage.py makemigrations
-python manage.py migrate
-python manage.py superuser
